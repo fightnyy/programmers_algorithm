115fd032ade3e34ca21d6958b4314b9a3a3bd2a7
fightnyy <fightnyy@naver.com>
2021-02-11 01:58:41 +0900

    biggest

---
+++ biggest_num.py
@@ -0,0 +1,16 @@
+import itertools
+import functools
+
+def solution(numbers):
+    def compare(a,b):
+        t1 = a+b
+        t2 = b+a
+        return (int(t1)>int(t2)) - (int(t2)>int(t1))
+    str_num = list(map(str,numbers))
+    # possble_per=list(itertools.permutations(str_num))
+    # str_num=["".join(s_num) for s_num in possble_per]
+    # print(str_num)
+    # num = list(map(int,str_num))
+    # return str(max(num))
+    numbers=sorted(str_num ,key= functools.cmp_to_key(compare) ,reverse = True)
+    return str(int("".join(numbers)))
