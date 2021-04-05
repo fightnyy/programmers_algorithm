6b1ab4789f90cec549ef2325dd84a752da7ab93d
fightnyy <fightnyy@naver.com>
2021-04-05 11:44:32 +0900

    youngyun.py

--- graph/farest_node.py
+++ graph/farest_node.py
@@ -3,38 +3,32 @@ from pdb import set_trace
 import copy
 
 
-def bfs(graph, visited):
+def bfs(f, graph, visited):
     q = deque()
-    q.append(graph[1])
-    visited[0] = 1
     count = 0
-    cnt_dict = defaultdict(int)
-    while any(v == 0 for v in visited):
+    q.append([f, count])
+    while q:
         # set_trace()
-        for node_list in copy.deepcopy(q):
-            for node in node_list:
-                if visited[node-1]:
-                    continue
-                else:
-                    visited[node-1] = 1
-                    cnt_dict[node] = count + 1
-                    q.append(graph[node])
-        count += 1
-    max_value = max(cnt_dict.values())
-    return list(cnt_dict.values()).count(max_value)
+        node, cnt = q.popleft()
+        if visited[node-1] != -1:
+            continue
+        else:
+            visited[node-1] = cnt
+            for v in graph[node]:
+                q.append([v, cnt+1])
+    return visited
 
 
 def solution(n, edge):
-    visited = [0]*n
+    visited = [-1]*n
     answer = 0
+    cnt = 0
     graph = defaultdict(list)
     for start, end in edge:
         graph[start].append(end)
         graph[end].append(start)
-    answer = bfs(graph, visited)
-    return answer
-
-
-n = 6
-vertex = [[1, 2], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
-print(solution(n, vertex))
+    answer = bfs(1, graph, visited)
+    for val in visited:
+        if val == max(answer):
+            cnt += 1
+    return cnt
